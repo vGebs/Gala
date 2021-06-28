16b19d4032acdef5398eb9c5e36d801a4cf259d9
Vaughn <vaughn.geber33@gmail.com>
2021-06-28 17:59:31 -0600

    BackEnd Setup

---
+++ Cloud Functions/functions/src/index.ts
@@ -0,0 +1,14 @@
+//Firebase Cloud functions tutorial: https://www.youtube.com/watch?v=DYfP-UIKxH0&list=PLl-K7zZEsYLkPZHe41m4jfAxUi0JjLgSM
+//TypeScritp Handbook: https://www.typescriptlang.org/docs/handbook/intro.html
+//TypeScript Course tutorial: https://www.youtube.com/watch?v=BwuLxPH8IDs&t=92s
+//Learn Node.js: https://nodejs.dev/learn/introduction-to-nodejs
+
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
